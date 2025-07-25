926f04843c95eee15544a51cbd3ebc560bedd193
harryrismananda <108707428+harryrismananda@users.noreply.github.com>
2025-07-25 14:53:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// const search = (data, keyword) => {
+//   for (let i = 0; i < data.length; i++) {
+//     if (data[i] === keyword) {
+//       return data[i];
+//     }
+//   }
+// };
+
+
+// const productCard = {
+//   image: `baju_fix.png`,
+//   name: `Product Name`,
+//   price: `Product Price`,
+//   description: `Product Description`,
+//   rating: `Product Rating`
+// }
+
