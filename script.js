fbdeb240fbc4a3e625e3fccf86f7fb3c90a99810
sabianathallah <45496893+sabianathallah@users.noreply.github.com>
2025-07-25 22:50:09 +0700

    raw readFunction (backend)

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-// const search = (data, keyword) => {
-//   for (let i = 0; i < data.length; i++) {
-//     if (data[i] === keyword) {
-//       return data[i];
-//     }
-//   }
-// };
+// lets starttttt
+
+/*
+ READ DATA
+=========
+1. buat function readData
+2. di dalam function, buat 1 variabel (template) dengan nilai awal ""
+3. looping database untuk dapat perObj => {id, namaTugas, tanggal, status}
+4. di dalam looping, concate variabel template dengan <tr> </tr>
+5. di luar looping, get elemen container (#containerBody) kemudian innerHTML dengan variabel template
+*/
 
 
-// const productCard = {
-//   image: `baju_fix.png`,
-//   name: `Product Name`,
-//   price: `Product Price`,
-//   description: `Product Description`,
-//   rating: `Product Rating`
-// }
+function readData(inputData) {
 
+    let template=""
+
+    for (let i = 0; i < inputData.length; i++) {
+        let elemen = inputData[i];
+        
+        template +=
+            `<tr>
+                <td>${elemen.firstName}</td>
+                <td>${elemen.lastName}</td>
+                <td>${elemen.birthDate}</td>
+                <td>${elemen.latestEducation}</td>
+                <td>${elemen.gpa}</td>
+                <td>${elemen.workExperienceOne}</td>
+                <td>${elemen.workExperienceOneDesc}</td>
+                <td>${elemen.workExperienceTwo}</td>
+                <td>${elemen.workExperienceTwoDesc}</td>.  
+                <td>${elemen.skillList}</td>
+                <td>${elemen.certAndLicenses}</td>
+                <td>${elemen.certAndLicenses2}</td>
+                <td>${elemen.languageOne}</td>
+                <td>${elemen.languageOneProficiency}</td>.  
+                <td>${elemen.languageTwo}</td>
+                <td>${elemen.languageTwoProficiency}</td>
+            </tr>`
+    }
+
+    document.getElementById("mb-3").innerHTML = template
+return template
+}
+
+readData()
